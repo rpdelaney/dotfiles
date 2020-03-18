7da7d27c5df714a0b67fe2c378ced50a4f6fddd3
Ryan Delaney <ryan.delaney@gmail.com>
2020-03-17 19:42:28 -0700

    Formatting docker_containers.py rule

--- home/.config/thefuck/rules/docker_containers.py
+++ home/.config/thefuck/rules/docker_containers.py
@@ -1,8 +1,13 @@
+#
 # `docker images` is a thing, but `docker containers` isn't because of reasons
+#
+
 def match(command):
     return "docker: 'containers' is not a docker command." in command.output
 
+
 def get_new_command(command):
-    return 'docker container list'
+    return "docker container list"
+
 
 enabled_by_default = True
